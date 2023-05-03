fff4d4b39dbafde8683269cf8356ca1d7564514f
joq62 <jakim.leche@gmail.com>
2023-05-03 11:56:30 +0200

    created

---
+++ src/kube_appl_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc kube_appl public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(kube_appl_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    kube_appl_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
